292f92734822fc2a497558dc4c182cb74a3cf2d4
Wesley Sousa <wesleysousa1121@gmail.com>
2024-11-20 20:04:46 -0300

    Arquivos Main e Classes criados

---
+++ classes.py
@@ -0,0 +1,14 @@
+import pandas as pd
+class ArquivoInspecao:    
+    def __init__(self, caminho):
+        self.eUmCSV = self.ArquivoValido(caminho)
+        if self.eUmCSV:
+            self.dataframe = pd.read_csv(caminho, sep=";")
+            self.cabecalho = self.dataframe.head()
+            self.verifica = True if not self.dataframe.isna().values.any() and not self.cabecalho.empty else False
+        else:
+            raise Exception("O arquivo não é um CSV")
+        
+    def ArquivoValido(self, caminho):
+        with open(caminho, "r") as file:
+            return caminho.lower().endswith('.csv')
