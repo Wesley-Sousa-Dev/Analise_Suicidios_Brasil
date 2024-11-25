0e3510a78514ef3a068c0fbe846b876513490d82
LuanVCD <Luanvitorcd@gmail.com>
2024-11-25 05:16:18 -0300

    ADD: Métodos para inserção de valores em reg_periodo, arma_fogo e comentários desnecessários que parecem complementar o código

--- Classes/BancoDados.py
+++ Classes/BancoDados.py
@@ -165,7 +165,7 @@ class BancoDados:
             self.db_connect.rollback()
             return f"Erro ao inserir os dados: {e}"
 
-    def inserir_gen(self, homemDf, mulherDf, colunasGen, colunasPer):
+    def inserir_gen(self, homemDf, mulherDf, colunasPer):
         try:
             dadosMulher = mulherDf.rename(columns={'período':'ano', 'valor': 'quantidade'}, inplace=True)
             dadosMulher = mulherDf.merge(colunasPer, on='ano', how='left')
@@ -196,6 +196,69 @@ class BancoDados:
         except Exception as e:
             self.db_connect.rollback()
             return f"Erro ao inserir os dados: {e}"
+        
+    def inserir_reg(self, colunasReg, regiaoDf, colunasPer):
+        #O que esperasse ter em cada parâmetro passado:
+        #colunasReg: ['reg_cod', 'nome']
+        #regiaoDf: ['nome', 'período', 'valor']
+        #colunasPer: ['per_cod', 'ano']
+
+        try:
+            dadosReg = regiaoDf.copy()
+            dadosReg = dadosReg.rename(columns={'período':'ano', 'valor':'quantidade'})
+            dadosReg = dadosReg.merge(colunasReg, on='nome', how='left')
+            dadosReg = dadosReg.merge(colunasPer, on='ano', how='left')
+
+            dadosReg = dadosReg[['per_cod', 'reg_cod','quantidade']].to_records(index=False).tolist()
+            
+            query = """
+            INSERT INTO reg_periodo (per_cod, reg_cod, quantidade)
+            VALUES (%s, %s, %s);
+            """
+
+            with self.db_connect.cursor() as cursor:
+                cursor.executemany(query, dadosReg)
+
+            self.db_connect.commit()
+            return "Dados inseridos com sucesso!"
+
+        except Exception as e:
+            self.db_connect.rollback()
+            return f"Erro ao inserir os dados: {e}"
+
+    def inserir_dados_fogo(self, mulherFogoDf, homemFogoDf, colunasPer):
+        #O que esperasse ter em cada parâmetro passado:
+        #mulherFogoDf: ['período', 'valor']
+        #homemFogoDf: ['período', 'valor']
+        #colunasPer: ['per_cod', 'ano']
+
+        try:
+            dadosFog = mulherFogoDf.copy()
+
+            #Explicação rápida: suffixes evita conflitos de colunas com nomes duplicados adicionando um sufixo em ambos
+            #em outras palavras valor mulherFogoDf vira: valor_mulher e o do merge do homemFogo Df é inserido como: valor_homem
+            dadosFog = dadosFog.merge(homemFogoDf, on='período', how='left', suffixes=('_mulher', '_homem'))
+            
+            dadosFog['quantidade_total'] = dadosFog['valor_mulher'] + dadosFog['valor_homem']
+            dadosFog = dadosFog.rename(columns={'período':'ano'})
+            dadosFog = dadosFog.merge(colunasPer, on='ano', how='left')
+
+            dadosFog = dadosFog[['per_cod', 'quantidade_total']].to_records(index=False).tolist()
+
+            query = """
+            INSERT INTO arma_fogo (per_cod, quantidade_total)
+            VALUES (%s, %s);
+            """
+
+            with self.db_connect.cursor() as cursor:
+                cursor.executemany(query, dadosFog)
+
+            self.db_connect.commit()
+            return "Dados inseridos com sucesso!"
+
+        except Exception as e:
+            self.db_connect.rollback()
+            return f"Erro ao inserir os dados: {e}"
 
     def buscar_dados(self, chosentable):
         try:
@@ -209,11 +272,11 @@ class BancoDados:
                 case 'regiao':
                     query = "SELECT * FROM regiao;"
                 case 'arma_fogo':
-                    return 'Num foi criado ainda kkkkk'
+                    query = "SELECT * FROM arma_fogo;"
                 case 'gen_periodo':
-                    return
+                    query = "SELECT * FROM gen_periodo ORDER BY per_cod;"
                 case 'reg_periodo':
-                    return
+                    query = "SELECT * FROM reg_periodo ORDER BY per_cod;"
                 case _:
                     return "Table escolhida não existe."
 
