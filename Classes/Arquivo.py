962b96e1d8f4d48729063fe426708431a0561e20
Wesley Sousa <wesleysousa1121@gmail.com>
2024-12-30 20:03:58 -0300

    Remove: funções inutilizadas

--- Classes/Arquivo.py
+++ Classes/Arquivo.py
@@ -1,15 +1,17 @@
 import pandas as pd
-class Arquivo:    
+
+
+class Arquivo:
     def __init__(self, caminho):
         self.eUmCSV = self.arquivo_valido(caminho)
         if self.eUmCSV:
             self.dataframe = pd.read_csv(caminho, sep=";")
             self.cabecalho = self.dataframe.head()
-            self.verifica = not self.dataframe.isna().values.any() and not self.cabecalho.empty
+            self.verifica = (
+                not self.dataframe.isna().values.any() and not self.cabecalho.empty
+            )
         else:
             raise Exception(f"O arquivo {caminho.split("/")[-1]} não é um CSV!")
-        
+
     def arquivo_valido(self, caminho):
-        with open(caminho, "r") as file:
-            return caminho.lower().endswith('.csv')
-        
+        return caminho.lower().endswith(".csv")
